054a8531e69ed00050e0bd94b6ce1783d3d718ae
haliluluoglu <haliluluoglu@gmail.com>
2019-02-27 21:19:43 +0300

    3.Hafta
    The problem is finding frequency of letters in a small case word.

---
+++ Yapisal Programlamaya Giris/frequencyOfLetters.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+	static int frequency[25];
+	int i=0,index;
+	char string[100];
+	
+	printf("Enter a word, please: ");
+	gets(string);
+	while(string[i]!='\0')
+	{
+		if((string[i]<='z')&&(string[i]>='a'))
+		{
+			index=string[i]-'a';
+			frequency[index]++;
+		}
+		i++;
+ 	}
+	for(i=1; i<25	; i++)
+	{
+		if(frequency[i]!=0)
+		{
+			if(frequency[i]==1)
+			{
+				printf("There is %d %c letter.\n", (frequency[i]), ('a'+i));
+			}
+			else
+			{
+				printf("There are %d %c letters.\n", (frequency[i]), ('a'+i));
+			}
+		}
+	}
+	return 0;
+}
