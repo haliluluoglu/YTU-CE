23c6925346bac239c31e7a5508fa1368937d1894
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-03-01 22:39:57 +0300

    3.Hafta
    Extending  to linked list algorithm.

--- Veri Yapıları ve Algoritmaları/linkedList.c
+++ Veri Yapıları ve Algoritmaları/linkedList.c
@@ -1,17 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-Node *insertTop(int,Node *)
 
-typedef struct link{
+typedef struct linkedList{
 	int data;
-	struct link *next;
+	struct linkedList *next;
 }Node;
 
-int main(void)
+Node *insertTop(int,Node *);
+void printList(Node *);
+Node *insertAfter(int,int,Node *);
+Node *deleteValue(int,Node *);
+void reserveList(Node *);
+
+int main()
 {
-	//CREATING LINKED LIST...
+	int i,number;
+	Node *root=NULL;
+	root=(Node *)malloc(sizeof(Node));
+
+	printf("Enter a number for add to list, please: ");
+	scanf("%d", &number);
+	//Initizalition to list.
+	root->data=number;
+	root->next=NULL;
+	printList(root);
+	//Inserting some elements on the top.
+	root=insertTop(10,root);
+	printList(root);
+	//Inserting some elements into the middle.
+	root=insertAfter(15,10,root);
+	printList(root);
+	//Delete some elements in the linked list.
+	root=deleteValue(15,root);
+	printList(root);
+	//Reversing the linked lis
+	reverseList(root);
+	printList(root);
+
+	return 0;
 }
-//Inserting element at the top of linked list.
+//Printing the linked list.
+void printList(Node *root)
+{
+	while(root!=NULL)
+	{
+		printf("%d\t", root->data);
+		root=root->next;
+	}
+	printf("\n");
+}
+//Making all elements in the list reversal.
+void reverseList(Node *head)
+{
+	Node *temp;
+	Node *currentNode=head;
+	Node *prevNode=NULL;
+	while(currentNode!=NULL)
+	{
+		temp=currentNode->next;
+		currentNode->next=prevNode;
+		prevNode=currentNode;
+		currentNode=temp;
+	}
+}
+//Inserting element at the top of linked list..
 Node *insertTop(int num, Node *head)
 {
 	Node *newNode;
@@ -19,29 +71,25 @@ Node *insertTop(int num, Node *head)
 	newNode->data=num;
 	newNode->next=head;
 	head=newNode;
-	
-	return head;
 }
 //Inserting element between two elements of linked list.
-Node *insertAfter(int num, int prevNum,Node *head)
+Node *insertAfter(int num, int prevNum, Node *head)
 {
-	Node *currentNode=head;
-	Node *newNode;
+	Node *currentNode=head,*newNode;
+	newNode=(Node *)malloc(sizeof(Node));
 	while(currentNode->data!=prevNum)
 	{
 		currentNode=currentNode->next;
 	}
-	newNode=(Node *)malloc(sizeof(Node));
 	newNode->data=num;
 	newNode->next=currentNode->next;
-
+	currentNode->next=newNode;
 	return head;
 }
-//Deleting elements of linked list
-Node *deleteValue(int num, Node *head)
+//Deleting elements of linked list.
+Node *deleteValue(int num,Node *head)
 {
-	Node *currentNode=head;
-	Node *prev;
+	Node *currentNode=head,*prevNode;
 	while(currentNode!=NULL)
 	{
 		if(currentNode->data==num)
@@ -52,10 +100,10 @@ Node *deleteValue(int num, Node *head)
 			}
 			else
 			{
-				prev->next=currentNode->next;
+				prevNode->next=currentNode->next;
 			}
 		}
-		prev=currentNode;
+		prevNode=currentNode;
 		currentNode=currentNode->next;
 	}
 	return head;
