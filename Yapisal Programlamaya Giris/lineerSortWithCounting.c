8030f56c82656b07cec03ba5bd75fd2b8ae6409f
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-03-16 01:23:07 +0300

    4.Hafta
    Lineer sort algorithm using counting logic.

--- Yapisal Programlamaya Giris/lineerSortWithCounting.c
+++ Yapisal Programlamaya Giris/lineerSortWithCounting.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+//WORSE WAY
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int main()
@@ -37,6 +38,65 @@ int main()
 	while(i<size)
 	{
 
+		if(final[counting[i]]==array[i])
+		{
+			final[counting[i]+1]=array[i];
+		}
+  		else
+  		{
+			final[counting[i]]=array[i];
+  		}
+		i++;
+	}
+	for(i=0; i<size; i++)
+	{
+		printf("%d\t", final[i]);
+	}
+	return 0;
+}*/
+//BETTER WAY
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int *array,*counting,*final;
+	int i,j,size;
+
+	printf("Enter size of array: ");
+	scanf("%d", &size);
+
+	array=(int *)malloc(sizeof(int)*size);
+	printf("Enter elements of array.\n");
+	for(i=0; i<size; i++)
+	{
+		printf("Array[%d]: ", i);
+		scanf("%d", array+i);
+	}
+	counting=(int *)malloc(sizeof(int)*size);
+	final=(int *)malloc(sizeof(int)*size);
+	for(i=0; i<size; i++)
+	{
+		counting[i]=0;
+		final[i]=0;
+	}
+	for(i=0; i<size; i++)
+	{
+		for(j=0; j<i; j++)
+		{
+			if(array[i]>array[j])
+			{
+				counting[i]++;
+			}
+			else
+			{
+				counting[j]++;
+			}
+		}
+	}
+	i=0;
+	while(i<size)
+	{
 		if(final[counting[i]]==array[i])
 		{
 			final[counting[i]+1]=array[i];
