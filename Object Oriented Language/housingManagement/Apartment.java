fe9a0773cc3c39526ca4fd3474f17c9ff4399875
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-03-27 07:57:47 +0300

    Housing Management Example
    This algorithm use for managing university housing.

---
+++ Object Oriented Language/housingManagement/Apartment.java
@@ -0,0 +1,33 @@
+package housingManagement;
+
+import java.util.ArrayList;
+
+public class Apartment {
+	private int doorNumber;
+	private ArrayList<Room> rooms;
+	
+	public Apartment(int doorNumber)
+	{
+		this.doorNumber=doorNumber;
+		
+		Room r1 = new Room("Living Room");
+		Room r2 = new Room("Bed Room");
+		Room r3 = new Room("Kitchen");
+		
+		rooms = new ArrayList<Room>();
+		
+		rooms.add(r1);
+		rooms.add(r2);
+		rooms.add(r3);
+		
+	}
+	public void setDoorNumber(int doorNumber)
+	{
+		this.doorNumber=doorNumber;
+	}
+	public int getDoorNumber()
+	{
+		return doorNumber;
+	}
+	
+}
---
+++ Object Oriented Language/housingManagement/Room.java
@@ -0,0 +1,19 @@
+package housingManagement;
+
+public class Room {
+	private String name;
+	
+	public Room(String name)
+	{
+		this.name=name;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+
+}
