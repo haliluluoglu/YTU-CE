a2f811977dcdeef9c058a1be5549c5088692f812
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-04-03 23:53:33 +0300

    6.Hafta
    Simulation of a robot which it can move forward,back,right and left.

---
+++ Yapisal Programlamaya Giris/robot.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int stepNumber,size,i,temp;
+	
+	printf("Enter number of step: ");
+	scanf("%d", &stepNumber);
+	
+	printf("Enter rows and coloums of matrix: ");
+	scanf("%d" , &size);
+	
+	for(i=0; i<stepNumber; i++)
+	{
+		temp=rand()%4;
+	}
+}
