2c6189e0414a2104fc4c728e22a6b8a1b643b018
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-04-09 22:35:30 +0300

    1.vize
    Yapısal Programlama 1.vize 1.soru

---
+++ Yapisal Programlamaya Giris/bbg2_1_vize_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+float findMean(int matrix[][4],int N,int value,int inputChoise,int outputChoise,int *count)
+{
+	int i;
+	float summary=0;
+	*count=0;
+	
+	for(i=0; i<N; i++)
+	{
+		if(matrix[inputChoise][]==value)
+		{
+			summary+=
+		}
+	}
+}
+
+int main()
+{
+	
+}
