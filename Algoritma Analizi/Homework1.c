94a742d8151225bbb9c5f64175def850d7351d98
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-10-24 11:41:19 +0300

    Add files via upload

--- Algoritma Analizi/Homework1.c
+++ Algoritma Analizi/Homework1.c
@@ -4,92 +4,87 @@
 
 typedef struct node
 {
-	float xApsis[MAX];
-	float yOrdinat[MAX];
+	int xApsis[MAX];
+	int yOrdinat[MAX];
 	
 }POINTS;
 
 
 
 void swap(int *num1, int *num2);
-void quickSort(float array[], int low, int high);
+void quickSort(int array[], int low, int high);
 
 void main()
 {
 	int sizeOfCoordinates; //N adet kullanýcý tarafýndan verilen nokta kordinatlarý sayýsý
 	int i; //For için kullanýlan deðiþken
-	
-	POINTS *first = (POINTS *) malloc(sizeof(POINTS) * sizeOfCoordinates);
+	POINTS first;
 	
 	printf("How many X coordinates? ");
 	scanf("%d" , &sizeOfCoordinates);
 	
+	
+	
+	
 	for(i=0; i<sizeOfCoordinates; i++)
 	{
 		printf("Enter %d. X coordinate, please " , (i+1));
-		scanf("%f", &(first->xApsis[i]));
+		scanf("%d", &(first.xApsis[i]));
 		printf("Enter %d. Y coordinate, please " , (i+1));
-		scanf("%f", &(first->yOrdinat[i]));
+		scanf("%d", &(first.yOrdinat[i]));
 		printf("\n");
 	}
 	printf("regular\n");
 	for(i=0; i<sizeOfCoordinates; i++)
 	{
-		printf("%f , %f \n" , (first->xApsis[i]) , (first->yOrdinat[i]));
+		printf("%d , %d \n" , (first.xApsis[i]) , (first.yOrdinat[i]));
 	}
-	
-	quickSort(first->xApsis, 0, sizeOfCoordinates-1);
+	printf("NEXT\n");
+	quickSort(first.xApsis, 0, sizeOfCoordinates-1);
 	
 	for(i=0; i<sizeOfCoordinates; i++)
 	{
-		printf("%f , %f \n" , (first->xApsis[i]) , (first->yOrdinat[i]));
+		printf("%d \n" , (first.xApsis[i]) );
 	}
 	
 }	
 
 
-void quickSort(float array[], int low, int high)
+void quickSort(int array[],int low,int high)
 {
-	int i,j,pivot;
-	
-	//printf("bastir: %d - %d\n" , low,high);
-	
+    int i,j,pivot;
+    int temp;
 
-	
-	if(low<high)
-	{
-		i=low;
-		j=high;
-		pivot=array[low];
-		
-		while(i<j)
-		{
-			
-			while((array[i]<=pivot )&&(i<high))	
-			{
-				i++;
-			}
-			while(array[j]>pivot)
-			{
-				j--;
-			}
-			if(i<j)
-			{
-				swap(&array[j], &array[i]);
-			}
-			
-		}
-		swap(&pivot,&array[j]);
-			for(i=0; i<=high; i++)
-	{
-		printf("cekkk");
-		printf("%f\n" , array[i]);
-	}
-		quickSort(array,low,j-1);
-		quickSort(array,j+1,high);
-		}
+     if(low < high){
+         pivot = low;
+         i = low;
+         j = high;
+
+         while(i < j)
+		 {
+             while(array[i] <= array[pivot] && i < high)
+             {
+             	i++;
+			 }
+             while(array[j] > array[pivot])
+             {
+             	j--;
+			 }
+             if(i <j)
+			 {
+                  temp = array[i];
+                  array[i] = array[j];
+                  array[j] = temp;
+             }
+         }
+         //swap(&array[pivot],&array[j]);
+         temp = array[pivot];
+         array[pivot] = array[j];
+         array[j] = temp;
+         quickSort(array,low,j-1);
+         quickSort(array,j+1,high);
+    }
 }
-
 void swap(int *num1, int *num2)
 {
 	int temp;
