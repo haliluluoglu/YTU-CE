cb66580450a1d17e7f13be9ca39582105ff8e75f
Halil Ibrahim Uluoglu <haliluluoglu@gmail.com>
2019-11-18 23:25:14 +0300

    9.Hafta
    Prototyping a funciton in header file

---
+++ Yapısal Programlama/helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "helloWorld.h"
+
+
+void yazdir(void)
+{
+	printf("Hello World!");
+}
+
+void main()
+{
+	
+}
---
+++ Yapısal Programlama/helloWorld.h
@@ -0,0 +1,4 @@
+#ifndef __helloWorld_h__
+#define __helloWorld_h__
+void yazdir(void);
+#endif
